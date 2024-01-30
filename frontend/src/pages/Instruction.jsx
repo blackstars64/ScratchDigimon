779031ea2284920c32b9514bd065ec7df0c57334
Blackstars <nelson.64@live.fr>
2024-01-30 14:17:23 +0100

    Add authentication check to pages

--- frontend/src/pages/Instruction.jsx
+++ frontend/src/pages/Instruction.jsx
@@ -1,4 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import terriermon from "../assets/terriermon.gif";
+
 function Instruction() {
+  const { token } = useContext(AuthContext);
+  if (!token) {
+    const navigate = useNavigate();
+    setTimeout(() => {
+      navigate("/");
+    }, 1000);
+    return (
+      <section className="app-start">
+        <h1> DigiScratch </h1>
+        <p className="redirecting">you're not logged in...</p>
+        <div className="app-img-btn">
+          <img className="app-terriermon" src={terriermon} alt="terriermon" />
+        </div>
+      </section>
+    );
+  }
   return <p>Instruction</p>;
 }
 
