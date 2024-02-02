a6c6bf5ecb46accd98d26ea14e6af14acb6eca12
Blackstars <nelson.64@live.fr>
2024-02-02 11:10:14 +0100

    Refactor LoseGame and WinGame components, add PropTypes, and update Instruction page styling

--- frontend/src/components/LoseGame.jsx
+++ frontend/src/components/LoseGame.jsx
@@ -1,5 +1,27 @@
-function LoseGame() {
-  return <p>LoseGame</p>;
+import PropTypes from "prop-types";
+
+function LoseGame({ clickRestart, setGetDigiPoint }) {
+  return (
+    <div className="lose-game">
+      <h1 className="lose-title">You Lose</h1>
+      <button
+        type="button"
+        className="solution-btn"
+        onClick={() => {
+          window.location.reload();
+          setGetDigiPoint(400);
+          clickRestart();
+        }}
+      >
+        Restart
+      </button>
+    </div>
+  );
 }
+
+LoseGame.propTypes = {
+  clickRestart: PropTypes.func.isRequired,
+  setGetDigiPoint: PropTypes.func.isRequired,
+};
 
 export default LoseGame;
--- frontend/src/components/WinGame.jsx
+++ frontend/src/components/WinGame.jsx
@@ -1,5 +1,35 @@
-function WinGame() {
-  return <p>WinGame</p>;
+import PropTypes from "prop-types";
+
+function WinGame({ clickRestart, digimon, setGetDigiPoint }) {
+  return (
+    <div className="lose-game">
+      <h1 className="win-title">You Win</h1>
+      <div className="win-c-img">
+        <img src={digimon.img} alt={digimon.name} className="win-img" />
+        <p className="win-dname">{digimon.name}</p>
+      </div>
+      <button
+        type="button"
+        className="solution-btn"
+        onClick={() => {
+          window.location.reload();
+          setGetDigiPoint(400);
+          clickRestart();
+        }}
+      >
+        Restart
+      </button>
+    </div>
+  );
 }
+
+WinGame.propTypes = {
+  clickRestart: PropTypes.func.isRequired,
+  digimon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+  }).isRequired,
+  setGetDigiPoint: PropTypes.func.isRequired,
+};
 
 export default WinGame;
